0c800c8a5502089f4567c265fc5e91b4a55d11b5
srepho <stephen.j.a.oates@gmail.com>
2014-01-21 20:46:52 +1100

    adding Code

---
+++ code.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+library(caret)
+train<-read.csv("F:/Loans/train.csv", header=T)
+test<-read.csv("F:/Loans/test.csv", header=T)
+sampsub<-read.csv("F:/Loans/sampleSubmission.csv", header=T)
