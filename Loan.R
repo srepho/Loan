bc9d3ae857df1c2bc5348fd6bfd41413af7d2e43
srepho <stephen.j.a.oates@gmail.com>
2014-01-22 00:21:41 +1100

    function

--- Loan.R
+++ Loan.R
@@ -3,3 +3,6 @@ library(caret)
 train<-read.csv("F:/Loans/train.csv", header=T)
 test<-read.csv("F:/Loans/test.csv", header=T)
 sampsub<-read.csv("F:/Loans/sampleSubmission.csv", header=T)
+function clean(){
+  
+}
