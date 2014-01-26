8274d755130fb458b66008394401f2d87d65c717
srepho <stephen.j.a.oates@gmail.com>
2014-01-26 19:52:19 +1100

    Update

--- Loan.R
+++ Loan.R
@@ -1,5 +1,7 @@
 library(caret)
-pp<-preProcess(train, method=c("BoxCox", "medianImpute"))
+loss<-train$loss
+train$loss<-NULL
+pp<-preProcess(train, method=c("BoxCox", "medianImpute", "scale", "center"))
 ntrain<-predict(pp, train)
 ntest<-predict(pp, test)
 nzv<-nearZeroVar(ntrain)
@@ -9,6 +11,6 @@ traincorr<-cor(fntrain)
 hcorr<-findCorrelation(traincorr)
 fnt<-fntrain[, -hcorr]
 fntest<-fntest[,-hcorr]
-loss<-train$loss
 trn<-cbind(fnt, loss)
 trnIndex<-createDataPartition()
+model<-train(trn$loss~., data=trn, method="enet", trControl=trainControl(method="repeatedcv", repeats=5))
